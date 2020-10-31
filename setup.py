f24d8e431c8c6b2a6cc41a772de9a1a7d21f9365
Roshan Shrestha <roshan_pra@hotmail.com>
2020-10-31 11:20:25 +0545

    another version for pip

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 # Import needed function from setuptools
 from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 # Create proper setup to be used by pip
 setup(name='kmersfasta',
-      version='0.0.1',
+      version='0.0.2',
       python_requires='>3.6.0',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       description='Display and count k-mers from the fasta file',
       author='Roshan Shrestha',
       packages=['kmersfasta'],
