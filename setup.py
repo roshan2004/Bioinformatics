039afb90992744db89c371f03af9d09586e873af
Roshan Shrestha <roshan_pra@hotmail.com>
2020-10-31 11:08:38 +0545

    setup for pip

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup
 
 # Create proper setup to be used by pip
-setup(name='kmers',
+setup(name='kmersfasta',
       version='0.0.1',
       python_requires='>3.6.0',
-      description='Display and count k-mers from the fast file',
+      description='Display and count k-mers from the fasta file',
       author='Roshan Shrestha',
-      packages=['kmers'],
+      packages=['kmersfasta'],
       install_requires=['biopython'])
 
